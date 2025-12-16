ebe118a858127e3d847ba7a05db9456384dfe013
Sallah26 <143meski@gmail.com>
2025-12-16 04:27:40 +0300

    feat: Implement comprehensive blog functionality including dynamic pages, pagination, and dedicated components

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -6,13 +6,37 @@ import { createClient } from '@/prismicio';
 
 export default async function page() {
   const client = createClient();
-  const posts = await client.getAllByType('blog_post');
-  console.log('blogs , ', posts);
+  const posts = await client.getAllByType('blog_post', {
+    orderings: {
+      field: 'my.blog_post.published_at',
+      direction: 'desc',
+    },
+  });
+
+  const blogs = posts.map((post) => ({
+    id: post.id,
+    title: post.data.title as string,
+    description: post.data.description as string,
+    date: new Date(post.data.published_at as string).toLocaleDateString(
+      'en-US',
+      {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+      }
+    ),
+    image: post.data.cover.url || '/images/property-1.png', // Fallback image
+    slug: (post.data.slug as string) || post.uid,
+  }));
+
+  const featuredBlogs = blogs.slice(0, 4);
+  const listedBlogs = blogs.slice(4);
+
   return (
     <main>
       <Home />
-      <FeaturedLists />
-      <BlogsList />
+      <FeaturedLists blogs={featuredBlogs} />
+      <BlogsList blogs={listedBlogs} />
       <AgentBanner />
     </main>
   );
