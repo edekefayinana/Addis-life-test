52b9570e48ea6a710a107fd94dd685e540424f9c
fayinana <fekedeananiya@gmail.com>
2026-02-08 14:55:05 +0300

    add close icon to header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
-import { Menu } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import {
